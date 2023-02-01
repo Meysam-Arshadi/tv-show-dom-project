932c99a320e21fdd3bf7111bfda44475a92242cd
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-02-01 20:25:29 +0000

    worked on CSS

--- script.js
+++ script.js
@@ -3,10 +3,34 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
+window.onload = setup;
+
+// const rootElem = document.getElementById("root");
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  episodeList.forEach((episode) => {
+    let episodeCard = document.createElement("div");
+    episodeCard.setAttribute("id", "episode-card");
 
-window.onload = setup;
+    let episodeNumber = document.createElement("p3");
+    episodeNumber.innerHTML = `S0${episode.season}E0${episode.number}`;
+    episodeNumber.setAttribute("id", "episode-number");
+    episodeCard.appendChild(episodeNumber);
+
+    let episodeTitle = document.createElement("h3");
+    episodeTitle.innerHTML = `${episode.name}`;
+    episodeTitle.setAttribute("id", "episode-title");
+    episodeCard.appendChild(episodeTitle);
+
+    let episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
+    episodeImage.setAttribute("id", "episode-img");
+    episodeCard.appendChild(episodeImage);
+
+    let episodeSummery = document.createElement("p");
+    episodeSummery.innerHTML = episode.summary;
+    episodeCard.appendChild(episodeSummery);
+
+    document.querySelector("#root").appendChild(episodeCard);
+  });
+}
